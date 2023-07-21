d4b26a652f9cc137d29eaf41367b69698f3b77b7
zee467 <wura.mahmud@gmail.com>
2023-07-21 16:13:54 +0100

    User login and logout with sessions

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, g, request
+from flask import Flask, render_template, g, request, session, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from database import get_db
-
+import os
 
 app = Flask(__name__)
+
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["DEBUG"] = True
 
 # closes down database 
@@ -16,7 +19,10 @@ def close_db(err):
 
 @app.route("/")
 def index():
-    return render_template("home.html") 
+    # checks if there is a user key in session
+    user = session.get('user', None)
+
+    return render_template("home.html", user=user) 
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -43,11 +49,13 @@ def login():
         user_result = user_cur.fetchone()
 
         if check_password_hash(user_result['password'], password):
+            session['user'] = user_result['name']
             return f"<h1>The password is correct!"
         else:
             return f"<h1>The password is incorrect!"
 
     return render_template("login.html")
+
 
 @app.route("/question")
 def question():
@@ -68,3 +76,10 @@ def unanswered():
 @app.route("/users")
 def users():
     return render_template("users.html")
+
+
+@app.route("/logout")
+def logout():
+    # removes user from session
+    session.pop('user', None)
+    return redirect(url_for('index'))
